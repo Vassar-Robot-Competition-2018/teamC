5618035718691981e5568fc1d895e4e1645c1d71
Frank Swiatowicz <fxswiatowicz@vassar.edu>
2018-02-11 20:22:15 -0500

    updated week 2 code

--- code/week_2_code/week_2_code.ino
+++ code/week_2_code/week_2_code.ino
@@ -1,28 +1,44 @@
-// This code was adapted from: https://www.allaboutcircuits.com/projects/servo-motor-control-with-an-arduino/
-
 #include <Servo.h> //Servo library
 
 const int SERVO_1 = 44;
 const int SERVO_2 = 45;
 const int IR = A0;
+int state = 0;
 
 //initialize two servo objects for the connected servos
-Servo servo_test_1;   
-Servo servo_test_2;               
- 
-void setup() 
-{ 
+Servo servo_test_1;
+Servo servo_test_2;
+
+void setup()
+{
   Serial.begin(9600);
-//  servo_test_1.attach(SERVO_1); // attach the signal pin of servo to pin44 of arduino
-//  servo_test_2.attach(SERVO_2); // attach the signal pin of servo to pin45 of arduino
-} 
-  
-void loop() 
-{                          
-//    servo_test_1.write(45); //command to rotate the servo to the specified angle
-//    servo_test_2.write(180);
-    int sensor_val = analogRead(IR);
-    Serial.println(sensor_val);
-    delay(500);                       
-} 
+
+  servo_test_1.attach(SERVO_1); // attach the signal pin of servo to pin44 of arduino
+  servo_test_2.attach(SERVO_2); // attach the signal pin of servo to pin45 of arduino
+
+}
+
+void loop()
+{
+
+  int sensor_val = analogRead(IR);
+  Serial.println(sensor_val);
+
+  //    servo_test_1.write(45); //command to rotate the servo to the specified angle
+  //      servo_test_2.write(135);
+
+  while ((sensor_val >= 80) && (state == 0)) {
+    servo_test_1.write(45); //command to rotate the servo to the specified angle
+    servo_test_2.write(135);
+    delay(250);
+    return;
+  }
+  if ((sensor_val < 80) && (sensor_val > 0) && (state == 0)) {
+    servo_test_1.write(135); //command to rotate the servo to the specified angle
+    servo_test_2.write(45);
+    sensor_val = 0;
+    state = 1;
+  }
+  if (state == 1) {
+    delay(3000);
  
