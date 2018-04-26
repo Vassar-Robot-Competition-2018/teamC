b465bdfb4656274ae9a1850693ac3c490326ee02
Frank Swiatowicz <fxswiatowicz@vassar.edu>
2018-04-26 12:45:02 -0400

    week-11-code update

--- code/week_11_code/week_11_code.ino
+++ code/week_11_code/week_11_code.ino
@@ -20,8 +20,8 @@ const int SERVO_1 = 31; //left servo
 const int SERVO_2 = 12; //right servo
 const int SERVO_MICRO = 2; //'lasso' servo
 const int LED_PIN = 22; //LED for sense_blocks()
-const int IR_FRONT = A3;
-const int BLOCK = 170;
+const int IR_FRONT = A0;
+const int BLOCK = 500;
 const int RED_PIN = 8; // Red RGB LED pin
 const int GREEN_PIN = 9; // Green RGB LED pin
 const int BLUE_PIN = 10; // Blue RGB LED pin
@@ -34,6 +34,10 @@ const int RED_Q = 4;
 
 //initial quadrant state set to 0
 int target_color = 0;
+int current_color = 0;
+
+// set to true when the robot has a block in its possession
+boolean has_block = false;
 
 //initialize two servo objects for the connected servos
 Servo servo_test_1;
@@ -62,6 +66,7 @@ void setup() {
   servo_test_1.attach(SERVO_1); // attach the signal pin of servo to pin44 of arduino
   servo_test_2.attach(SERVO_2); // attach the signal pin of servo to pin45 of arduino
   micro_serve.attach(SERVO_MICRO);
+  micro_serve.write(90);
   // set each pin on the RGB LED to output
   pinMode(BLUE_PIN, OUTPUT);
   pinMode(RED_PIN, OUTPUT);
@@ -72,16 +77,23 @@ void setup() {
 }
 
 void loop() {
-  //drive();
-  //  detect_quadrant_left();
-  //  delay(1000);
-  //  detect_quadrant_right();
-  //  //border_left();
-  //  //border_right();
-  //  int front_sensor_val = analogRead(IR_FRONT);
-  //  sense_blocks(front_sensor_val);
+  drive();
+//  detect_quadrant_left();
+//  detect_quadrant_right();
+//  if ((detect_quadrant_left() > 0) && (detect_quadrant_right() == 0)) {
+//    straighten_left();
+//    delay(1000);
+//  }
+//  else if ((detect_quadrant_left() == 0) && (detect_quadrant_right() > 0)) {
+//    straighten_right();
+//    delay(1000);
+//  }
+//
+//
+//  border_left();
+//  border_right();
   int front_sensor_val = analogRead(IR_FRONT);
-  get_sensor_value(front_sensor_val);
+  sense_blocks(front_sensor_val);
 
 }
 
@@ -103,107 +115,140 @@ void rotate() {
   servo_test_2.write(45);
 }
 
-void detect_quadrant_left() {
+void kill_servos() {
+  servo_test_1.write(90);
+  servo_test_2.write(90);
+}
+
+int detect_quadrant_left() {
   //getting values from left RGB sensor
   uint16_t clear, red, green, blue, colorTemp, lux;
   tcs1.getRawData(&red, &green, &blue, &clear);
   lux = tcs1.calculateLux(red, green, blue);
   //until robot reaches tape, drive
-  if (target_color == 0) {
-    //drive();
-    //when lux is above 2000, the sensor is on the tape
-    if (lux > 2000) {
-      //yellow tape conditions
-      if ((red > blue) && (green > blue) && ((red - blue) > 1000)) {
-        //set the RGB LED to yellow
-        setColor(255, 50, 0);
-        //change state to yellow
+  //when lux is above 2000, the sensor is on the tape
+  if (lux > 2000) {
+    Serial.println("LUX > 2000");
+    //yellow tape conditions
+    if ((red > blue) && (green > blue) && ((red - blue) > 1000)) {
+      //set the RGB LED to yellow
+      setColor(255, 50, 0);
+      //change state to yellow
+      if (target_color == 0) {
         target_color = YELLOW_Q;
       }
-      //white tape conditions
-      else if ((red > 9000) && (blue > 9000) && (green > 9000)) {
-        // back up, wait, then rotate
-        reverse();
-        delay(1000);
-        rotate();
-        delay(2000);
-        return;
-      }
-      //green tape conditions
-      else if ((green > red) && (green > blue)) {
-        //set the RGB LED to green
-        setColor(0, 255, 0);
-        //change state to green
+      current_color = YELLOW_Q;
+      return current_color;
+    }
+    //white tape conditions
+    else if ((red > 9000) && (blue > 9000) && (green > 9000)) {
+      // back up, wait, then rotate
+      reverse();
+      delay(1000);
+      rotate();
+      delay(2000);
+      return;
+    }
+    //green tape conditions
+    else if ((green > red) && (green > blue)) {
+      //set the RGB LED to green
+      setColor(0, 255, 0);
+      //change state to green
+      if (target_color == 0) {
         target_color = GREEN_Q;
       }
-      //red tape conditons
-      else if ((red > green) && (red > blue)) {
-        //set the RGB LED to red
-        setColor(255, 0, 0);
-        //change state to red
+      current_color = GREEN_Q;
+      return current_color;
+    }
+    //red tape conditions
+    else if ((red > green) && (red > blue)) {
+      //set the RGB LED to red
+      setColor(255, 0, 0);
+      //change state to red
+      if (target_color == 0) {
         target_color = RED_Q;
       }
-      //blue tape conditions
-      else if ((blue > green) && (blue > red)) {
-        //set the RGB LED to blue
-        setColor(0, 0, 255);
-        //change state to blue
+      current_color = RED_Q;
+      return current_color;
+    }
+    //blue tape conditions
+    else if ((blue > green) && (blue > red)) {
+      //set the RGB LED to blue
+      setColor(0, 0, 255);
+      //change state to blue
+      if (target_color == 0) {
         target_color = BLUE_Q;
       }
+      current_color = BLUE_Q;
+      return current_color;
+    }
+  }
+}
+int detect_quadrant_right() {
+  //getting values from left RGB sensor
+  uint16_t clear, red, green, blue, colorTemp, lux;
+  tcs2.getRawData(&red, &green, &blue, &clear);
+  lux = tcs2.calculateLux(red, green, blue);
+  //until robot reaches tape, drive
+  //when lux is above 2000, the sensor is on the tape
+  if (lux > 2000) {
+    Serial.println("LUX > 2000");
+    //yellow tape conditions
+    if ((red > blue) && (green > blue) && ((red - blue) > 1000)) {
+      //set the RGB LED to yellow
+      setColor(255, 50, 0);
+      //change state to yellow
+      if (target_color == 0) {
+        target_color = YELLOW_Q;
+      }
+      current_color = YELLOW_Q;
+      return current_color;
+    }
+    //white tape conditions
+    else if ((red > 9000) && (blue > 9000) && (green > 9000)) {
+      // back up, wait, then rotate
+      reverse();
+      delay(1000);
+      rotate();
+      delay(2000);
+      return;
+    }
+    //green tape conditions
+    else if ((green > red) && (green > blue)) {
+      //set the RGB LED to green
+      setColor(0, 255, 0);
+      //change state to green
+      if (target_color == 0) {
+        target_color = GREEN_Q;
+      }
+      current_color = GREEN_Q;
+      return current_color;
+    }
+    //red tape conditions
+    else if ((red > green) && (red > blue)) {
+      //set the RGB LED to red
+      setColor(255, 0, 0);
+      //change state to red
+      if (target_color == 0) {
+        target_color = RED_Q;
+      }
+      current_color = RED_Q;
+      return current_color;
+    }
+    //blue tape conditions
+    else if ((blue > green) && (blue > red)) {
+      //set the RGB LED to blue
+      setColor(0, 0, 255);
+      //change state to blue
+      if (target_color == 0) {
+        target_color = BLUE_Q;
+      }
+      current_color = BLUE_Q;
+      return current_color;
     }
   }
 }
 
-void detect_quadrant_right() {
-  //getting values from the right RGB sensor
-  uint16_t clear, red, green, blue, lux;
-  tcs2.getRawData(&red, &green, &blue, &clear);
-  lux = tcs2.calculateLux(red, green, blue);
-  //until robot reaches tape, drive
-  if (target_color == 0) {
-    drive();
-    //when lux is above 2000, the sensor is on the tape
-    if (lux > 2000) {
-      //yellow tape conditions
-      if ((red > blue) && (green > blue) && ((red - blue) >= 1000)) {
-        //set the RGB LED to yellow
-        setColor(255, 50, 0);
-        //change state to yellow
-        target_color = YELLOW_Q;
-      }
-      //white tape conditions
-      else if ((red > 9000) && (blue > 9000) && (green > 9000)) {
-        // back up, wait, then rotate
-        reverse();
-        delay(1000);
-        rotate();
-        delay(2000);
-        return;
-      }
-      //green tape conditions
-      else if ((green > red) && (green > blue)) {
-        //set the RGB LED to green
-        setColor(0, 0, 255);
-        //change state to green
-        target_color = GREEN_Q;
-      }
-      //red tape conditions
-      else if ((red > green) && (red > blue)) {
-        //set the RGB LED to red
-        setColor(255, 0, 0);
-        //change state to red
-        target_color = RED_Q;
-      }
-      //blue tape conditions
-      else if ((blue > green) && (blue > red)) {
-        //set the RGB LED to blue
-        setColor(0, 0, 255);
-        //change state to blue
-        target_color = BLUE_Q;
-      }
-    }
-  }
-}
 
 void border_left() {
   //get left RGB sensor values
@@ -240,7 +285,7 @@ void border_right() {
 }
 
 void sense_blocks(int front_sensor_val) {
-  //intialize variable to store number of objects Pixycam sees
+  //initialize variable to store number of objects Pixycam sees
   int num_blocks = pixy.getBlocks();
   //get left RGB sensor values (temporary)
   uint16_t clear, red, green, blue, lux;
@@ -249,36 +294,40 @@ void sense_blocks(int front_sensor_val) {
   /*check each object detected by pixycam to see if it
      matches thae target color (and robot is not out of bounds)
   */
-  for (int i = 0; i < num_blocks; i++) {
-    if ((pixy.blocks[i].signature == target_color) & !((red > 9000) && (blue > 9000) && (green > 9000) && (lux > 3000))) {
-      Serial.println("Found a block/Not on white tape");
-      //signal that a block matching target color was found
-      signal_block();
-      //if object is to the left, turn left
-      if (pixy.blocks[i].x < 130) {
-        servo_test_1.write(45);
-        servo_test_2.write(110);
-        delay(250);
+  while (front_sensor_val > BLOCK) {
+    for (int i = 0; i < num_blocks; i++) {
+      if ((pixy.blocks[i].signature == target_color) & !((red > 9000) || (blue > 9000) || (green > 9000) || (lux > 3000))) {
+        Serial.println("Found a block/Not on white tape");
+        //signal that a block matching target color was found
+        signal_block();
+        //if object is to the left, turn left
+        if (pixy.blocks[i].x < 130) {
+          servo_test_1.write(45);
+          servo_test_2.write(110);
+          delay(250);
+        }
+        //if object is in central view, drive straight
+        else if ((pixy.blocks[i].x >= 130) && (pixy.blocks[i].x <= 160)) {
+          servo_test_1.write(45);
+          servo_test_2.write(135);
+          delay(250);
+        }
+        //if object is to the right, turn right
+        else if (pixy.blocks[i].x > 160) {
+          servo_test_1.write(70);
+          servo_test_2.write(135);
+          delay(250);
+        }
       }
-      //if object is in central view, drive straight
-      else if ((pixy.blocks[i].x >= 130) && (pixy.blocks[i].x <= 160)) {
-        servo_test_1.write(45);
-        servo_test_2.write(135);
-        delay(250);
+      //if there are no blocks matching the target color, stop (temporary)
+      else {
+        kill_servos();
       }
-      //if object is to the right, turn right
-      else if (pixy.blocks[i].x > 160) {
-        servo_test_1.write(70);
-        servo_test_2.write(135);
-        delay(250);
-      }
-    }
-    //if there are no blocks matching the target color, stop (temporary)
-    else {
-      servo_test_1.write(90);
-      servo_test_2.write(90);
     }
   }
+  has_block = true;
+  drop_lasso();
+  drive_home();
 }
 
 void drop_lasso() {
@@ -307,4 +356,57 @@ void setColor(int red, int green, int blue)
   analogWrite(GREEN_PIN, green);
   analogWrite(BLUE_PIN, blue);
 }
+
+void straighten_left() {
+  while ((detect_quadrant_left() > 0) && (detect_quadrant_right() == 0)) {
+    servo_test_1.write(90);
+    servo_test_2.write(135);
+    delay(100);
+  }
+}
+
+void straighten_right() {
+  while ((detect_quadrant_left() == 0) && (detect_quadrant_right() > 0)) {
+    servo_test_1.write(45);
+    servo_test_2.write(90);
+    delay(5);
+  }
+}
+
+void drive_home() {
+  while (current_color != target_color) {
+    drive();
+    border_left();
+    border_right();
+    straighten_left();
+    straighten_right();
+  }
+  delay(1000);
+  kill_servos();
+  lift_lasso();
+}
+
+
+
+
+//void model() {
+// int home_q = target_color;
+//  switch (home_q) {
+//    case 1: return_to_blue();
+//            break;
+//    case 2: return_to_yellow();
+//            break;
+//    case 3: return_to_green();
+//            break;
+//    case 4: return_to_red();
+//            break;
+//  }
+//}
+//
+//void return_to_blue() {
+//  int cur_q = current_color;
+//  switch (cur_q) {
+//
+//  }
+//}
 
